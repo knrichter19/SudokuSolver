99a6fef220a8f4944f7e368ff84c53c7185bacba
knrichter19 <knrichter19@coe.edu>
2021-11-12 17:15:35 -0600

    more learning tkinter

--- Visualizer.py
+++ Visualizer.py
@@ -3,25 +3,24 @@ import tkinter as tk
 class Visualizer:
     def __init__(self):
         root = tk.Tk()
-        #root.geometry("500x700")
-        buttonFrame = tk.Frame(root, width = 500, height = 200)
+        root.geometry("500x700")
+        buttonFrame = tk.Frame(root, width = 500, height = 200, bg = "blue")
         buttonFrame.pack(side=tk.BOTTOM)
-
-        sudokuFrame = tk.Frame(root, width = 500, height = 500)
-        sudokuFrame.pack()
-
-
         testButton = tk.Button(buttonFrame, width = 10, height = 5)
         testButton.pack()
-        self.grid = []
 
+        self.grid = []
+        sudokuFrame = tk.Frame(root, width = 500, height = 500, bg = "yellow")
         for i in range(9):
+            sudokuFrame.columnconfigure(i, weight = 1, minsize = 5)
+            sudokuFrame.rowconfigure(i, weight = 1, minsize = 3)
             row = []
             for j in range(9):
-                sudokuSquare = tk.Entry(sudokuFrame, bg = "green", width = 2)
+                sudokuSquare = tk.Text(sudokuFrame, bg = "green")
                 row.append(sudokuSquare)
                 sudokuSquare.grid(column = j, row = i)
             self.grid.append(row)
+        sudokuFrame.pack()
         root.mainloop()
 
 
